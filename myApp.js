245382c0887eb919c30eec6647be023ad41d8020
Jean-Christophe LAFLEUR <fabrice.972.20@gmail.com>
2025-06-24 13:32:55 +0200

    hsts

--- myApp.js
+++ myApp.js
@@ -5,10 +5,11 @@ helmet = require('helmet');
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({ action: 'deny' }));
 app.use(helmet.xssFilter());
-
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 
+timeInSeconds = 90*24*60*60;
+app.use(helmet.hsts({ maxAge: timeInSeconds ,  force : true}));
 
 
 
