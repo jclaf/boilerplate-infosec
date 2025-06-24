de4acd7002592e7823f0ae5a698b79f0c1a92c49
Jean-Christophe LAFLEUR <fabrice.972.20@gmail.com>
2025-06-24 15:56:28 +0200

    bcrypt

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-const helmet = require('helmet');
+// const helmet = require('helmet');
 
 
 
@@ -29,31 +29,31 @@ const helmet = require('helmet');
 
 // Version 2 - simplify
 
-app.use(helmet({
-  hidePoweredBy: true,
+// app.use(helmet({
+//   hidePoweredBy: true,
   
-  frameguard: {         // configure
-    action: 'deny'
-  },
+//   frameguard: {         // configure
+//     action: 'deny'
+//   },
  
-  noSniff: true,
-  xssFilter: true,
-  ieNoOpen: true,
+//   noSniff: true,
+//   xssFilter: true,
+//   ieNoOpen: true,
 
-  hsts: { 
-    maxAge: 90*24*60*60 ,
-    force : true
-  },
+//   hsts: { 
+//     maxAge: 90*24*60*60 ,
+//     force : true
+//   },
 
-  dnsPrefetchControl: true,
-  noCache: true,
-  contentSecurityPolicy: {    // enable and configure
-    directives: {
-      defaultSrc: ["'self'"],
-      scriptSrc: ["'self'",'trusted-cdn.com'],
-    }
-  },
-}))
+//   dnsPrefetchControl: true,
+//   noCache: true,
+//   contentSecurityPolicy: {    // enable and configure
+//     directives: {
+//       defaultSrc: ["'self'"],
+//       scriptSrc: ["'self'",'trusted-cdn.com'],
+//     }
+//   },
+// }))
 
 
 
