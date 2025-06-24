70301b7203516e0d7f558beb77168ec0cb2d367b
Jean-Christophe LAFLEUR <fabrice.972.20@gmail.com>
2025-06-24 13:54:14 +0200

    CSP

--- myApp.js
+++ myApp.js
@@ -16,6 +16,12 @@ app.use(helmet.dnsPrefetchControl());
 app.use(helmet.noCache());
 
 
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'","'trusted-cdn.com'"],
+  }
+}));
 
 
 
