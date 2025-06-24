2ce331a7a5dd8627e3d67cf82262d13ab3a19f85
Jean-Christophe LAFLEUR <fabrice.972.20@gmail.com>
2025-06-24 15:06:10 +0200

    bcrypt

--- myApp.js
+++ myApp.js
@@ -1,47 +1,31 @@
 const express = require('express');
 const app = express();
 
-const helmet = require('helmet');
+//const helmet = require('helmet');
 const bcrypt = require('bcrypt');
 
 
 // Version 1
-app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({ action: 'deny' }));
+// app.use(helmet.hidePoweredBy());
+// app.use(helmet.frameguard({ action: 'deny' }));
 // app.use(helmet.xssFilter());
 // app.use(helmet.noSniff());
 // app.use(helmet.ieNoOpen());
 
-timeInSeconds = 90*24*60*60;
-app.use(helmet.hsts({ maxAge: timeInSeconds ,  force : true}));
+// timeInSeconds = 90*24*60*60;
+// app.use(helmet.hsts({ maxAge: timeInSeconds ,  force : true}));
 
-app.use(helmet.dnsPrefetchControl());
+// app.use(helmet.dnsPrefetchControl());
 
 // app.use(helmet.noCache());
 
 
-app.use(helmet.contentSecurityPolicy({
-  directives: {
-    defaultSrc: ["'self'"],
-    scriptSrc: ["'self'",'trusted-cdn.com'],
-  }
-}));
-
-
-const saltRounds = 12; // Secure cost factor
-
-// Example usage: Hash a password (for demonstration)
-const myPlaintextPassword = 'superSecret123';
-
-bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
-  if (err) {
-    console.error('Error hashing password:', err);
-  } else {
-    console.log('Hashed password:', hash);
-    // You can store this hash in your database
-  }
-});
-
+// app.use(helmet.contentSecurityPolicy({
+//   directives: {
+//     defaultSrc: ["'self'"],
+//     scriptSrc: ["'self'",'trusted-cdn.com'],
+//   }
+// }));
 
 // Version 2 - simplify
 
